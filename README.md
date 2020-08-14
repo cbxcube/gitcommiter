cf3dc09e8a478f136842735cf20d2c7646c0febd
cbxcube <cubbed@gmail.com>
2020-08-14 23:25:05 +0200

    Commited on Fri Aug 14 23:25:05 CEST 2020

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gitcommiter
 collect data about system and push to git hourly
+
+- location on kimsufi: ~/production/gitcommiter
+  user: cbx
+
+# crontab
+ 
+# run the reporter script 30 minutes after each hour
+30 * * * * /software/scripts/reporter.sh
+
+# run every 2 hours at 0 minutes
+0 */2 * * * /software/scripts/parse_status.pl
---
+++ reporter.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+
+reprotfile="collectstat.csv"
+archivefile=$(date)${reportfile}
+
+
+function populate_data {
+	date >> $reportfile
+	uptime >> $reportfile
+	df -h / >> $reportfile
+	df -h /data >> $reportfile
+	echo "data colection completed"
+	mv $reportfile $(date)$reportfile
+	echo "file renamed"
+}
+
+function commitgit {
+    git add .
+    git commit -m "Commited on $(date)" -a
+    git push
+    echo "data pushed to git"
+}
+
+# runtime
+populate_data
+commitgit
+echo "program completed. Exiting."
+exit 0
